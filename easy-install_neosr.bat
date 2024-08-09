4bc7c1fd09eb6d405918c617d24fa04796853684
ScreamingLlama <56514044+starinspace@users.noreply.github.com>
2024-08-09 07:22:48 +0200

    Add files via upload

--- easy-install_neosr.bat
+++ easy-install_neosr.bat
@@ -8,9 +8,9 @@ timeout /t 1
 
 %USERPROFILE%\miniconda3\Scripts\activate.bat %USERPROFILE%\miniconda3
 
-conda create --name NeoSR python=3.11
+conda create --name NeoSR python=3.12
 conda activate NeoSR
-conda install pytorch=2.1.2 torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
+conda install pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
 git clone https://github.com/muslll/neosr
 cd neosr
 pip install -e .
