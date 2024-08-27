95a666d9750ebda52c869ecb0f77f096dfbdeb98
ScreamingLlama <56514044+starinspace@users.noreply.github.com>
2024-08-27 18:30:40 +0200

    Add files via upload

--- update-one-click-install.bat
+++ update-one-click-install.bat
@@ -2,5 +2,5 @@
 :: Turn off command echoing to keep the output clean
 
 :: Define URLs for the batch files
-curl "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/run-neosr.bat"
-curl "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/update-neosr.bat"
+curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/run-neosr.bat"
+curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/update-neosr.bat"
