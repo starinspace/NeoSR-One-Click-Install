716743a4079e79771b966c67faab5a81a1e83d57
ScreamingLlama <56514044+starinspace@users.noreply.github.com>
2024-08-27 17:25:29 +0200

    Add files via upload
    run-neosr got a facelift.
    update-neosr will not retrieve python version from version.txt and dependencies.

--- update-neosr.bat
+++ update-neosr.bat
@@ -1,13 +1,34 @@
 @echo off
 
+:: Download and update NeoSR-One-Click-Install using curl
+curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/run-neosr.bat"
+
+:: Download version.txt from GitHub to retrieve the required Python version and dependencies
+curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/version/version.txt"
+
+:: Read Python version and packages from version.txt
+for /f "tokens=1,2 delims==" %%a in (version.txt) do (
+    if "%%a"=="python" set "PYTHON_VERSION=%%b"
+    if "%%a"=="packages" set "PACKAGES=%%b"
+)
+
+:: Activate the Conda environment
 call "%USERPROFILE%\miniconda3\Scripts\activate.bat" "%USERPROFILE%\miniconda3"
 call conda activate NeoSR
 
+:: Pull the latest changes from the Git repository
 git pull origin master
 
-conda install python=3.12
-conda install pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
+:: Install the required Python version
+conda install python=%PYTHON_VERSION%
 
+:: Install the required packages
+for %%p in (%PACKAGES%) do (
+    conda install %%p
+)
+
+:: Install Python packages
 pip install -e .
 pip install packaging
+
 pause
