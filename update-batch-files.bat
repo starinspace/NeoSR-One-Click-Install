2ddb76665de63553d8bf15f9aa5d25aa62833433
ScreamingLlama <56514044+starinspace@users.noreply.github.com>
2024-08-27 20:36:15 +0200

    Delete update-batch-files.bat

--- update-batch-files.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Turn off command echoing to keep the output clean
-
-:: Define URLs for the batch files
-curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/launch-neosr.bat"
-curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/update-neosr.bat"
