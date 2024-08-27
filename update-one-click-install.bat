b8c547ccf5ea383d4f60e408edb02744288fc190
ScreamingLlama <56514044+starinspace@users.noreply.github.com>
2024-08-27 19:24:46 +0200

    Update update-one-click-install.bat

--- update-one-click-install.bat
+++ update-one-click-install.bat
@@ -2,5 +2,5 @@
 :: Turn off command echoing to keep the output clean
 
 :: Define URLs for the batch files
-curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/run-neosr.bat"
+curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/launch-neosr.bat"
 curl -O "https://raw.githubusercontent.com/starinspace/NeoSR-One-Click-Install/main/update-neosr.bat"
